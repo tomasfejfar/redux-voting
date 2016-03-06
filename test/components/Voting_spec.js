cd5cc56df7ecd6aeee8f4ccb27ebf10efc356bd8
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-03-06 23:32:59 +0000

    Voting

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -6,7 +6,17 @@ import {expect} from 'chai';
 
 describe('Voting', () => {
     it('displays two buttons', () => {
-        const wrapper = shallow(<Voting pair={["Trainspotting", "28 Days Later"]} />);
-        expect(wrapper.find('button')).to.have.length(2);
+        const wrapper = shallow(<Voting pair={['Trainspotting', '28 Days Later']} />);
+        var buttons = wrapper.find('button');
+        expect(buttons).to.have.length(2);
+        expect(buttons.at(0).text()).to.equal('Trainspotting');
+        expect(buttons.at(1).text()).to.equal('28 Days Later');
+    });
+
+    it('invokes callback when a button is clicked', () => {
+        let votedWith;
+        const vote = (entry) => votedWith = entry;
+        const wrapper = shallow(<Voting pair={['Trainspotting', '28 Days Later']} vote={vote} />);
+
     });
 });
