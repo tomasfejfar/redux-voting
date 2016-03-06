e06b99a6aa7aa4c23a6e775a484e48e587f346a9
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-03-06 00:18:48 +0000

    React init + hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        './src/index.js',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server'
+    ],
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module : {
         loaders : [{
             test: /\.jsx$/,
             exclude: /node_modules/,
-            loader: babel
+            loader: 'react-hot!babel'
         }]
     },
     resolve : {
