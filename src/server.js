ce7e296a2b23d97759548885d489ac9df9d45cab
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-03-12 23:53:58 +0000

    Fixed missing function call

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import Server from "socket.io";
 
 export default function startServer(store) {
     const io = new Server().attach(8090);
+    console.log('Server running on localhost:8090');
 
     store.subscribe(
-        () => io.emit('state', store.getState.toJS())
+        () => io.emit('state', store.getState().toJS())
     );
 
     io.on('connection', (socket) => {
-        socket.emit('state', store.getState.toJS());
+        socket.emit('state', store.getState().toJS());
         socket.on('action', store.dispatch.bind(store));
     });
 }
