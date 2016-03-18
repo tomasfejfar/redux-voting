d8f33382bacc84fbcaa859f2cbca759dbb0ebbb2
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-03-18 19:20:06 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "mocha"
+  ],
+  "env": {
+    "mocha": true
+  },
+  "rules": {
+    "react/prefer-es6-class": [2, 'never'],
+    "react/prop-types": [0],
+    "react/prefer-stateless-function": [0],
+    "no-console": [0],
+    "quote-props": [0],
+    "new-cap": [
+      2,
+      {
+        // for Immutable
+        "capIsNewExceptions": [
+          "List",
+          "OrderedSet",
+          "Range",
+          "Map",
+        ]
+      }
+    ],
+  },
+};
